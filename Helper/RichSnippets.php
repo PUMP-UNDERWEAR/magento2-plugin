b6dd4e31acf624dae501ea93253b3e90d9f5ca00
Sam-PUMP <sam@pumpunderwear.com>
2018-08-05 01:12:33 -0400

    Update RichSnippets.php

--- Helper/RichSnippets.php
+++ Helper/RichSnippets.php
@@ -1,20 +1,16 @@
 <?php
-
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 namespace Yotpo\Yotpo\Helper;
-
 class RichSnippets extends \Magento\Framework\App\Helper\AbstractHelper
 {
-
     private $_config;
     private $_model;
     private $_helper;
     protected $_storeManager;    
-
     public function __construct(
             \Yotpo\Yotpo\Block\Config $config,
             \Yotpo\Yotpo\Model\Richsnippet $model,
@@ -26,9 +22,7 @@ class RichSnippets extends \Magento\Framework\App\Helper\AbstractHelper
         $this->_helper = $helper;
         $this->_storeManager = $storeManager;
     }
-
     public function getRichSnippet() {
-
         try {
             
             $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
@@ -42,29 +36,24 @@ class RichSnippets extends \Magento\Framework\App\Helper\AbstractHelper
             if (($snippet == null) || (!$snippet->isValid())) {
                 //no snippet for product or snippet isn't valid anymore. get valid snippet code from yotpo api
                 $res = $this->_helper->createApiGet("products/" . ($this->_config->getAppKey()) . "/richsnippet/" . $productId, 2);
-
                 print_r(json_encode($res));
                 if ($res["code"] != 200) {
                     //product not found or feature disabled.
                     return "";
                 }
-
                 $body = $res["body"];
                 $averageScore = $body->response->rich_snippet->reviews_average;
                 $reviewsCount = $body->response->rich_snippet->reviews_count;
                 $ttl = $body->response->rich_snippet->ttl;
-
                 if ($snippet == null) {
                     $snippet = $this->_model;
                     $snippet->setProductId($productId);
                     $snippet->setStoreId($storeId);
                 }
-
                 $snippet->setAverageScore($averageScore);
                 $snippet->setReviewsCount($reviewsCount);
                 $snippet->setExpirationTime(date('Y-m-d H:i:s', time() + $ttl));
                 $snippet->save();
-
                 return array("average_score" => $averageScore, "reviews_count" => $reviewsCount);
             }
             return array("average_score" => $snippet->getAverageScore(), "reviews_count" => $snippet->getReviewsCount());
@@ -75,5 +64,3 @@ class RichSnippets extends \Magento\Framework\App\Helper\AbstractHelper
     return true;
         
     }
-
-}
